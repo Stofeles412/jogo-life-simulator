eb478d6148d579343492510935d872f8eabbd43d
Stofeles412 <fernandostofeles@gmail.com>
2025-03-26 10:51:56 -0300

    .

---
+++ jogo/script.js
@@ -0,0 +1,32 @@
+let pontos = 0;
+let xp = 0;
+let nivel = 1;
+
+const pontosEl = document.getElementById('pontos');
+const xpEl = document.getElementById('xp');
+const nivelEl = document.getElementById('nivel');
+const logEl = document.getElementById('log');
+
+function realizarTarefa(tarefa, pontosGanhos, xpGanho) {
+  pontos += pontosGanhos;
+  xp += xpGanho;
+  log(`Você fez "${tarefa}" e ganhou ${pontosGanhos} pontos e ${xpGanho} XP.`);
+
+  if (xp >= nivel * 100) {
+    xp -= nivel * 100;
+    nivel++;
+    log(`🎉 Parabéns! Você subiu para o nível ${nivel}!`);
+  }
+
+  atualizarStatus();
+}
+
+function atualizarStatus() {
+  pontosEl.textContent = pontos;
+  xpEl.textContent = xp;
+  nivelEl.textContent = nivel;
+}
+
+function log(mensagem) {
+  logEl.innerHTML = `<p>${mensagem}</p>` + logEl.innerHTML;
+}
